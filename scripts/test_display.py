9212761bde60e3251abe9e6cbca039f456399bd5
marshine520 <307612483@qq.com>
2020-05-14 10:38:55 +0800

    firest commit

---
+++ scripts/test_display.py
@@ -0,0 +1,20 @@
+import os, sys
+sys.path.append(os.getcwd())
+
+from base.base_driver import init_driver
+from page.display_page import DisplayPage
+
+
+class TestDisplay:
+
+    def setup(self):
+        self.driver = init_driver()
+        self.display_page = DisplayPage(self.driver)
+
+    def test_mobile_display_input(self):
+        # 点击放大镜
+        self.display_page.click_search()
+        # 输入文字
+        self.display_page.input_search_text("1")
+        # 点击返回
+        self.display_page.click_back()
